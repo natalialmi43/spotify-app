b840d141465f4b879c5be9d6bf71b475d0f78c0f
Natalia <natalia.marcos@educa.jcyl.es>
2026-02-23 23:39:16 +0100

    create the data repository, here we call to the data bases and implements de interface

---
+++ src/features/albums/data/AlbumDataRepository.java
@@ -0,0 +1,20 @@
+package features.albums.data;
+
+import features.albums.domain.Album;
+import features.albums.domain.AlbumRepository;
+
+import java.util.ArrayList;
+
+public class AlbumDataRepository implements AlbumRepository {
+
+    private AlbumMemLocalDataSource albumMemLocalDataSource;
+
+    public AlbumDataRepository(AlbumMemLocalDataSource albumMemLocalDataSource) {
+        this.albumMemLocalDataSource = albumMemLocalDataSource;
+    }
+
+    @Override
+    public ArrayList<Album> getAlbums() {
+        return albumMemLocalDataSource.findAll();
+    }
+}
