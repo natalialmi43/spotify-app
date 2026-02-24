ce0c00032805e07d62b3f90de821655e4635cd2b
Natalia <natalia.marcos@educa.jcyl.es>
2026-02-24 23:07:48 +0100

    create save author use case

---
+++ src/features/songs/domain/SaveSongUseCase.java
@@ -0,0 +1,14 @@
+package features.songs.domain;
+
+public class SaveSongUseCase {
+
+    private SongRepository songRepository;
+
+    public SaveSongUseCase(SongRepository songRepository) {
+        this.songRepository = songRepository;
+    }
+
+    public void execute (Song song){
+        songRepository.saveSong(song);
+    }
+}
